4d1b383991bceb61d415f4eadada9120b96f12b5
acechua <acecyq@gmail.com>
2018-07-24 17:45:00 +0800

    adding all

--- db/migrate/20180724072436_create_cases.rb
+++ db/migrate/20180724072436_create_cases.rb
@@ -5,7 +5,6 @@ class CreateCases < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :specialization
       t.text :description
-      t.references :client, foreign_key: true
       
       t.timestamps
     end
