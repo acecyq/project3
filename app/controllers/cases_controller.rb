dbe1734d613d31e09746cc65513f3a16270e2677
Shaunprawn <shaun.seah@live.com.sg>
2018-07-25 14:01:23 +0800

    cases controller and views for edit and new

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,17 +1,48 @@
 class CasesController < ApplicationController
+
+before_action :find_case, only: [:edit, :update, :delete]
+
     def create
+        @case = Case.new(case_params)
+        # @client = Client.find(params[:client_id]).id
+        # @case.client_id << @client
+        @case.save
+        redirect_to '/'
     end
 
     def new
+        @case = Case.new
     end
 
     def edit
+        @case = Case.find(params[:id])
     end
 
-    def delete
+    def update
+        @case = Case.find(params[:id])
+        @case.update(case_params)
+        redirect_to '/'        
+    end
+
+    def destroy
+        @case.destroy
+
+        redirect_to '/'
     end
 
     def show
+        # should show case by id layout - case details followed by client details
+
+    end
+
+private
+
+    def case_params
+        params.require(:case).permit(:title, :specialization, :description, :client_id)
     end
     
+    def find_case
+        @case = Case.find(params[:id])
+    end
+
 end
