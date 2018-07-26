a0e4ccafdfb55211ab93063b98e3acd9bf27b4e9
acechua <acecyq@gmail.com>
2018-07-26 19:46:29 +0800

    edit seed file to include all specializations and more instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,25 +16,64 @@
 
 
 # run rails db:drop, db:create and db:migrate
-# sign up 4 users before running this seed file
+# sign up 8 users before running this seed file
 
-Lawyer.create(name: "Law1", number: "12345678", description: "Lawyer number 1", photo: "https://www.koreaboo.com/wp-content/uploads/2017/09/songjoongki1.jpg", firm: "LawOne Pte Ltd", address: "1 Claymore Hill", user_id: 1)
-Lawyer.create(name: "Law2", number: "87654321", description: "Lawyer number 2", photo: "https://showtimelive.co.uk/wp-content/uploads/2018/05/Sean-Paul-High-Definition-Wallpapers-.jpg", firm: "LawOne Pte Ltd", address: "2 Claymore Hill", user_id: 2)
-Client.create(name: "Client1", number: "97487234", user_id: 3)
-Client.create(name: "Client2", number: "92328737", user_id: 4)
+Lawyer.create(name: "Law1", number: "83242383", description: "Lawyer number 1", photo: "https://www.koreaboo.com/wp-content/uploads/2017/09/songjoongki1.jpg", firm: "LawOne Pte Ltd", address: "1 Claymore Hill", user_id: 1)
+Lawyer.create(name: "Law2", number: "87654321", description: "Lawyer number 2", photo: "https://showtimelive.co.uk/wp-content/uploads/2018/05/Sean-Paul-High-Definition-Wallpapers-.jpg", firm: "LawTwo Pte Ltd", address: "2 Claymore Hill", user_id: 2)
+Lawyer.create(name: "Law3", number: "92347892", description: "Lawyer number 3", photo: "https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/brunomars-hero_0.jpg?itok=NxVLunrf", firm: "LawThree Pte Ltd", address: "3 Claymore Hill", user_id: 3)
+Lawyer.create(name: "Law4", number: "97897834", description: "Lawyer number 4", photo: "https://cdn.images.dailystar.co.uk/dynamic/58/photos/976000/620x/Lionel-Messi-718661.jpg", firm: "LawFour Pte Ltd", address: "4 Claymore Hill", user_id: 4)
+Client.create(name: "Client1", number: "82768347", user_id: 5)
+Client.create(name: "Client2", number: "82347264", user_id: 6)
+Client.create(name: "Client3", number: "82746827", user_id: 7)
+Client.create(name: "Client4", number: "83746284", user_id: 8)
 Case.create(title: "Case1", specialization: "Accidents", description: "Accident 1", client_id: 1)
 Case.create(title: "Case2", specialization: "Arbitration", description: "Arbitration 1", client_id: 2)
+Case.create(title: "Case3", specialization: "Banking and Finance", description: "Banking and Finance 1", client_id: 3)
+Case.create(title: "Case4", specialization: "Bribery and Anti-Corruption", description: "Bribery and Anti-Corruption 1", client_id: 4)
+Case.create(title: "Case5", specialization: "Construction", description: "Construction 1", client_id: 1)
+Case.create(title: "Case6", specialization: "Contracts", description: "Contracts 1", client_id: 2)
+Case.create(title: "Case7", specialization: "Corporate and Commercial", description: "Corporate and Commercial 1", client_id: 3)
+Case.create(title: "Case8", specialization: "Criminal Defence", description: "Criminal Defence 1", client_id: 4)
 
 specializations = [
 	"Accidents",
 	"Arbitration",
 	"Banking and Finance",
 	"Bribery and Anti-Corruption",
-	"Business Crime and Fraud"
+	"Business Crime and Fraud",
+	"Construction",
+	"Contracts",
+	"Corporate and Commercial",
+	"Criminal Defence",
+	"Debt Recovery",
+	"Employment and Benefits (Businesses)",
+	"Employment and Benefits (Individuals)",
+	"Environmental Law",
+	"Family and Matrimonial",
+	"Financial Services",
+	"Immigration",
+	"Insolvency (Personal)",
+	"Insurance (Businesses)",
+	"Insurance (Individuals)",
+	"Intellectual Property",
+	"International Arbitration",
+	"Investment Funds (Businesses)",
+	"Mediation (Businesses)",
+	"Shipping and Maritime",
+	"Tax",
+	"Technology, Media and Telecoms",
+	"Wealth Management",
+	"Wills, Probate and Trusts"
 ]
 specializations.each do |s|
 	Specialization.create(name: s)
 end
 
 Specialization.find(1).lawyers << Lawyer.find(1)
+Specialization.find(4).lawyers << Lawyer.find(1)
 Specialization.find(2).lawyers << Lawyer.find(2)
+Specialization.find(3).lawyers << Lawyer.find(2)
+Specialization.find(5).lawyers << Lawyer.find(3)
+Specialization.find(7).lawyers << Lawyer.find(3)
+Specialization.find(6).lawyers << Lawyer.find(4)
+Specialization.find(8).lawyers << Lawyer.find(4)
