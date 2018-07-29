6c206b548b9d4025f7dd0de92b0e3a36a64027c8
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-07-29 13:35:02 +0800

    changed to flash message modals for notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def flash_class(level)
+        case level
+        when 'notice'  then 'modal is-active notification is-info'
+        when 'success' then 'modal is-active notification is-success'
+        when 'error'   then 'modal is-active notification is-danger'
+        when 'alert'   then 'modal is-active notification is-warning'
+        end
+    end
 end
